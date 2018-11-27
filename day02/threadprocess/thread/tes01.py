0f8f2a6e3fd69fe276b331cb05d7382ac8dc6c3e
linfeng <simple_days@qq.com>
2018-11-27 19:13:35 +0800

    add some comment in thread01

--- day02/threadprocess/thread/tes01.py
+++ day02/threadprocess/thread/tes01.py
@@ -13,6 +13,17 @@ def loop():
 
 print('thread %s is running...' % threading.current_thread().name)
 t = threading.Thread(target=loop, name='LoopThread')
+t2 = threading.Thread(target=loop, name='LoopThread02')
+'''
+t2.start()
+t2.join()
 t.start()
 t.join()
+线程02先运行完，后线程1才能运行
+'''
+t.start()
+t2.start()
+t.join()
+t2.join()
+# 线程t和t2交替执行
 print('thread %s ended.' % threading.current_thread().name)
